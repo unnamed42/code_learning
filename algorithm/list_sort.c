a112333e2b52132f4c4e6d42aa9713efe097ded7
unnamed42 <a390517426@gmail.com>
2016-03-29 15:53:15 +0800

    Merge sort for singly-linked list in C

--- algorithm/list_sort.c
+++ algorithm/list_sort.c
@@ -18,6 +18,7 @@ typedef struct l{
     return h;
 }*/
 
+// Insertion sort method
 list *insert_sort(list *head){
     if(head==NULL)
         return NULL;
@@ -40,3 +41,51 @@ list *insert_sort(list *head){
     }
     return head;
 } 
+
+// Merge sort method
+
+// merge_sort helper
+void merge(list *h1,list *h2){
+    list dummy;list *ptr=&dummy;
+    while(h1!=NULL&&h2!=NULL){
+        if(h1->data<h2->data){
+            ptr->next=h1;
+            h1=h1->next;
+        } else {
+            ptr->next=h2;
+            h2=h2->next;
+        }
+        ptr=ptr->next;
+    }
+    if(h1!=NULL)
+        ptr->next=h1;
+    if(h2!=NULL)
+        ptr->next=h2;
+    return dummy.next;
+}
+
+void merge_sort(list *head){
+    if(head==NULL||head->next==NULL)
+        return head;
+    if(head->next->next==NULL){
+        list *node=head->next;
+        if(node->data<head->data){
+            node->next=head;
+            head->next=NULL;
+            head=node;
+        }
+        return head;
+    }
+    list *slow=head,*fast=head->next->next;
+    while(fast!=NULL){
+        slow=slow->next;
+        fast=fast->next;
+        if(fast!=NULL)
+            fast=fast->next;
+    }
+    // Reuse fast
+    fast=merge_sort(slow->next);
+    slow->next=NULL;
+    return merge(merge_sort(head),fast);
+}
+
