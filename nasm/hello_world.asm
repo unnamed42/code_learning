a9673712982b48f73e3c3ba0e905f8f253a9dbd6
unnamed42 <a390517426@gmail.com>
2016-04-11 21:44:58 +0800

    Add details

--- nasm/hello_world.asm
+++ nasm/hello_world.asm
@@ -1,21 +1,25 @@
-%define STDOUT 1
-%define SYS_WRITE 4
-%define SYS_EXIT 1
+%define STDOUT 1        ;standard output file
+%define SYS_WRITE 4     ;system call write()
+%define SYS_EXIT 1      ;system call exit()
+%define EXIT_SUCCESS 0  ;EXIT_SUCCESS exit status
+
+global main             ;entry point main
+
+bits 32                 ;this program works in 32-bit protected mode
 
 section .data
-    msg db 'Hello, world'
+    msg db 'Hello, world!',0
     len equ $ - msg
 
 section .text
-    global main
 
 main:
-    mov eax,SYS_WRITE ;call sys_write
-    mov ebx,STDOUT    ;file write to (stdout)
-    mov ecx,msg       ;message to write
-    mov edx,len       ;length of message to write
-    int 0x80          ;execution
+    mov eax,SYS_WRITE    ;call sys_write
+    mov ebx,STDOUT       ;file write to (stdout)
+    mov ecx,msg          ;message to write
+    mov edx,len          ;length of message to write
+    int 0x80             ;execution
     
-    mov eax,SYS_EXIT  ;call exit
-    mov ebx,0         ;exit status
-    int 0x80          ;execution
+    mov eax,SYS_EXIT     ;call exit
+    mov ebx,EXIT_SUCCESS ;exit status
+    int 0x80             ;execution
